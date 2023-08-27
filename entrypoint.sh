021072f19328c796a028856efd042b967ad9356e
Alexey <redspirit@live.ru>
2023-08-27 12:31:27 +0700

    upd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-echo "Start photon"
-java -jar photon.jar -nominatim-import -host localhost -port 5432 -database nominatim -user nominatim -password qaIACxO6wMR3 -languages ru
+
+# Download elasticsearch index
+if [ ! -d "/photon/photon_data/elasticsearch" ]; then
+    echo "Downloading search index"
+
+    # Let graphhopper know where the traffic is coming from
+    USER_AGENT="docker: thomasnordquist/photon-geocoder"
+    wget --user-agent="$USER_AGENT" -O - https://download1.graphhopper.com/public/extracts/by-country-code/ru/photon-db-ru-latest.tar.bz2 | bzip2 -cd | tar x
+fi
+
+# Start photon if elastic index exists
+if [ -d "/photon/photon_data/elasticsearch" ]; then
+    echo "Start photon"
+    java -jar photon.jar $@
+else
+    echo "Could not start photon, the search index could not be found"
+fi
